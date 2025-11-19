c1c8c1d7e3236550b311fb53040b526f6d9faaaa
adaniesl2 <adaniesl@eafit.edu.co>
2025-11-19 17:58:56 -0500

    Update Inicio.py

--- varanalisis-main/Inicio.py
+++ varanalisis-main/Inicio.py
@@ -25,11 +25,7 @@ st.markdown("""
 
 # Title and description
 st.title('📊 Análisis de datos de Sensor de Dimmer de Luz 💡')
-st.header("Esta aplicación permite ver el historial y analizar datos de sensores recolectados en todas las :yellow[luces] que has conectado a tu cuenta.", divider="yellow")
-st.markdown("""
-    Esta aplicación permite ver el historial y analizar datos de sensores
-    recolectados en todas las luces que has conectado a tu cuenta.
-""")
+st.header("Esta aplicación permite ver el historial y :red[analizar datos] de sensores recolectados en todas las :yellow[luces] que has conectado a :blue[tu cuenta].", divider="yellow")
 
 # Create map data for EAFIT
 eafit_location = pd.DataFrame({
