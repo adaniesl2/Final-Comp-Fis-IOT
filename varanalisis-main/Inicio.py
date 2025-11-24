4a53ff3288fa4b0cdff7201c4000b546a9b5e6dd
adaniesl2 <adaniesl@eafit.edu.co>
2025-11-24 10:50:45 -0500

    Update Inicio.py

--- varanalisis-main/Inicio.py
+++ varanalisis-main/Inicio.py
@@ -11,14 +11,13 @@ st.set_page_config(
     layout="wide"
 )
 
-page_bg_img = '''
+page_bg_img = 
 <style>
 body {
 background-image: url("https://www.slashgear.com/img/gallery/why-america-banned-incandescent-light-bulbs/intro-1690819198.jpg");
 background-size: cover;
 }
 </style>
-'''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
